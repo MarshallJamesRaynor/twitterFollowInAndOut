ff0f8ce880c53c9d40696719689c382ea397db04
Jim Raynor <paolo@combi.li>
2016-12-04 17:17:56 +0100

    read config file and create a connection

--- transfert.php
+++ transfert.php
@@ -7,5 +7,11 @@ require "twitteroauth/autoload.php";
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
+$configIn = parse_ini_file('configIn.ini');
+$confiOut = parse_ini_file('configOut.ini');
+
+$connectionIn = new TwitterOAuth($configIn['consumer_key'],$configIn['consumer_secret'], $configIn['access_token'], $configIn['access_token_secret']);
+$connectionOut = new TwitterOAuth($confiOut['consumer_key'],$confiOut['consumer_secret'], $confiOut['access_token'], $confiOut['access_token_secret']);
+
 
 ?>
